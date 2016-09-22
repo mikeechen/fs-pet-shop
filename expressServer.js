4b5378f1d58874958a3feeb57581925da764c7ea
mikeechen <mikeechen915@gmail.com>
2016-09-22 13:21:14 -0700

    restful thing

--- expressServer.js
+++ expressServer.js
@@ -9,11 +9,26 @@ const port = process.env.PORT || 8000;
 const bodyParser = require('body-parser');
 
 app.disable('x-powered-by');
-fs.readFile(petsPath, 'utf8', (err, data) =>{
+
+function writeFile(petswr, petwr, reswr) {
+  fs.writeFile(petsPath, JSON.stringify(petswr), (writeErr) => {
+    if (writeErr) {
+      console.error(writeErr.stack);
+
+      return reswr.sendStatus(500);
+    }
+    res.send(petwr);
+  });
+}
+
+fs.readFile(petsPath, 'utf8', (err, data) => {
   const pets = JSON.parse(data);
+
   if (err) {
     console.error(err.stack);
-    return res.sendStatus(500);
+    app.use((req, res) => {
+      res.sendStatus(500);
+    });
   }
 
   app.use(bodyParser.json());
@@ -41,15 +56,34 @@ fs.readFile(petsPath, 'utf8', (err, data) =>{
 
     if (!isNaN(pet.age) && (pet.name !== undefined || pet.name !== '') && (pet.kind !== undefined || pet.kind !== '')) {
       pets.push(pet);
-      fs.writeFile(petsPath, JSON.stringify(pets), (writeErr) => {
-        if (writeErr) {
-          console.error(writeErr.stack);
-          return res.sendStatus(500);
-        }
-        res.send(pet);
-      });
+      writeFile(pets, pet, res);
     } else {
       return res.sendStatus(400);
+    }
+  });
+
+  app.put('/pets/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const pet = {
+      age: parseInt(req.body.age),
+      kind: req.body.kind,
+      name: req.body.name
+    };
+    if (id < 0 || id > pets.length) {
+      return res.sendStatus(400);
+    }
+    if (!isNaN(pet.age) && (pet.name !== undefined || pet.name !== '') && (pet.kind !== undefined || pet.kind !== '')) {
+      pets[id] = pet;
+      writeFile(pets, pet, res);
+    } else {
+      return res.sendStatus(400);
+    }
+  });
+
+  app.delete('/pets/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if () {
+
     }
   });
 
@@ -58,7 +92,7 @@ fs.readFile(petsPath, 'utf8', (err, data) =>{
   });
 });
 
-app.listen(port,() => {
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
--- httpServer.js
+++ httpServer.js
@@ -5,23 +5,23 @@ const fs = require('fs');
 const path = require('path');
 const petsPath = path.join(__dirname, 'pets.json');
 const port = process.env.PORT || 8000;
-//.indexOf('/pets') !== -1
 const server = http.createServer((req, res) => {
   fs.readFile(petsPath, 'utf8', (err, data) => {
     const pets = JSON.parse(data);
     const petRegExp = new RegExp(/^\/pets\/(.*)$/);
+
     if (req.method === 'GET' && req.url.indexOf('/pets') !== -1) {
       let indexNumber;
 
       if (petRegExp.test(req.url)) {
-      //  indexNumber = parseInt(req.url.substring(req.url.lastIndexOf('/') + 1));
-       indexNumber = parseInt(req.url.match(petRegExp)[1]);
+        indexNumber = parseInt(req.url.match(petRegExp)[1]);
       }
 
       if (err) {
         console.error(err.stack);
         res.statusCode = 500;
         res.setHeader('Content-Type', 'text/plain');
+
         return res.end('Internal Server Error');
       }
 
@@ -36,12 +36,12 @@ const server = http.createServer((req, res) => {
         res.setHeader('Content-Type', 'text/plain');
         res.end('Not Found');
       }
-
     } else if (req.method === 'POST' && req.url === '/pets') {
-      let obj = {};
+      const obj = {};
 
       req.on('data', function(chunk) {
         const tempobj = JSON.parse(chunk.toString());
+
         obj.age = parseInt(tempobj.age);
         obj.kind = tempobj.kind;
         obj.name = tempobj.name;
@@ -51,6 +51,7 @@ const server = http.createServer((req, res) => {
             console.error(err.stack);
             res.statusCode = 500;
             res.setHeader('Content-Type', 'text/plain');
+
             return res.end('Internal Server Error');
           }
           pets.push(obj);
@@ -59,6 +60,7 @@ const server = http.createServer((req, res) => {
               console.error(err.stack);
               res.statusCode = 500;
               res.setHeader('Content-Type', 'text/plain');
+
               return res.end('Internal Server Error');
             }
             res.setHeader('Content-Type', 'application/json');
